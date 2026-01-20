2a1c7370002bbc4a4c9660db7e030e4a3126736d
elazar meijers <meijers212@gmail.com>
2026-01-20 15:43:04 +0200

    add a router main .env and env_exemple files

--- service_a/app/router.py
+++ service_a/app/router.py
@@ -1,7 +1,20 @@
 from fastapi import APIRouter
+from modols import GetWeatherData
+import requests
+from dotenv import load_dotenv
+import os 
+
+load_dotenv()
+
+host = os.getenv("SERVIS_B_HOST")
+port = os.getenv("SERVIS_B_PORT")
+print(host,port)
 
 router = APIRouter()
 
 @router.post("/ingest")
-def post():
-    pass
+def post(location: str):
+    data = GetWeatherData.ingest_weather_for_location(location)
+    url = f"https://{host}/{port}"
+
+    x = requests.post(url, json = data)
