efba5b84440ad79d8e94fc6886047899367caeeb
elazar meijers <meijers212@gmail.com>
2026-01-21 10:16:09 +0200

    add a basic fuly fanctunal main for servic_c

---
+++ service_c/app/main.py
@@ -0,0 +1,22 @@
+from fastapi import FastAPI
+from router import router
+
+app = FastAPI()
+
+app.include_router(router)
+
+
+
+if __name__=="__main__":
+    import uvicorn
+    uvicorn.run("main:app", port=8080, host="localhost", reload=True)
+
+
+
+
+
+
+    
+
+
+
